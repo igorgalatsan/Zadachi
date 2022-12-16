8483264c5c1b917f51de6fa38cad1b5177fe81aa
igor <igorgalatsan@gmail.com>
2022-12-16 13:44:37 +0300

    Решил две задачи

--- hm_work_set.py
+++ hm_work_set.py
@@ -12,3 +12,24 @@ print(diff_num(some_list))
 
 
 #69 35 69 94 82 54 68 96 33 65 65
+
+# Даны два списка чисел, которые могут содержать до 100000 чисел каждый. 
+# Посчитайте, сколько чисел содержится одновременно как в первом списке,
+#  так и во втором. 
+
+
+def two_list(start_list_1:list,start_list_2:list) :
+    some_set_1 =set(start_list_1)
+    some_set_2 =set(start_list_2)
+    result_diff_1_2 =len(some_set_1.difference(some_set_2))
+    result_diff_2_1 = len(some_set_2.difference(some_set_1))
+    
+    return result_diff_1_2, result_diff_2_1
+
+some_list_1 = [69,35,69,94,82,54,68,96,33,65,65]
+some_list_2 = [69,35,69,94,82,55,66,99,34,63,64]
+ 
+print(f" в первом списке - {two_list(some_list_1,some_list_2)} во втором")
+
+# не понимаю красиво вывести на экран  :
+#  в первом списке - 5 элементов   во втором - 6 элементов
